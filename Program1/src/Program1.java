2b16858fadadd65210228614e12536923cd1dd05
BaelArtem <71084197+BaelArtem@users.noreply.github.com>
2020-09-13 21:56:10 +0300

    (a+b)

---
+++ Program1/src/Program1.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Program1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner sc = new Scanner(System.in);
+ int a,b,c;
+ System.out.println("¬ведите первое число");
+ a = sc.nextInt();
+ 
+ System.out.println("¬ведите второе число");
+ b = sc.nextInt();
+ 
+ c=a+b;
+ 
+ System.out.println("c = " +c);
+	}
+
+}
